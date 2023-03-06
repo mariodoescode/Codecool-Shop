621e73e8c4f97d8c72319a3c2bba2e18fedffcd0
Mario Rusu <106011177+mariodoescode@users.noreply.github.com>
2023-03-06 15:25:37 +0200

    solved sorting system

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -25,3 +25,21 @@ window.onclick = function(event) {
         }
     }
 }
+
+
+function myFunctionSup() {
+    document.getElementById("myDropdownSup").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
