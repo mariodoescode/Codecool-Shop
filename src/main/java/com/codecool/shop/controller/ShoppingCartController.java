6db119012c35374e5652c8979590f3681e2cc361
Mario Rusu <106011177+mariodoescode@users.noreply.github.com>
2023-03-07 14:12:08 +0200

    worked on setting quantity

--- src/main/java/com/codecool/shop/controller/ShoppingCartController.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCartController.java
@@ -33,31 +33,34 @@ public class ShoppingCartController extends HttpServlet {
         //  response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
-        if(request.getParameter("id") != null){
+        if (request.getParameter("id") != null) {
             int productId = Integer.parseInt(request.getParameter("id"));
+            System.out.println(productId);
             Product product = productDao.find(productId);
             shoppingCart.add(product);
-        } else if (request.getParameter("plus") != null) {
-            int productId = Integer.parseInt(request.getParameter("plus"));
-            Product product = productDao.find(productId);
-            product.setQuantity(product.getQuantity() + 1);
         }
-        else if (request.getParameter("minus") != null) {
-            int productId = Integer.parseInt(request.getParameter("minus"));
+        if (request.getParameter("quantity") != null) {
+            int quantity = Integer.parseInt(request.getParameter("quantity"));
+            int productId = Integer.parseInt(request.getParameter("id"));
             Product product = productDao.find(productId);
+            System.out.println(quantity);
+            product.setQuantity(quantity);
             if(product.getQuantity() <= 1){
                 shoppingCart.remove(productId);
-            }else{
-                product.setQuantity(product.getQuantity() - 1);
             }
         }
-        context.setVariable("products",shoppingCart.getAllProducts());
-        context.setVariable("totalPrice",shoppingCart.getTotalPrice());
+            context.setVariable("products", shoppingCart.getAllProducts());
+            context.setVariable("totalPrice", shoppingCart.getTotalPrice());
 
-        PrintWriter out = response.getWriter();
+            PrintWriter out = response.getWriter();
 
-        System.out.println(shoppingCart.getAllProducts());
+            System.out.println(shoppingCart.getAllProducts());
 
-        engine.process("product/cart.html", context, response.getWriter());
+            engine.process("product/cart.html", context, response.getWriter());
+        }
+
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
     }
+
 }
