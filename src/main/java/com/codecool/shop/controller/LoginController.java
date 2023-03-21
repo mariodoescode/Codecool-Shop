5992094d795516a44251b16e4610a0a1040cbab7
cristianfrancisc22 <105937581+cristianfrancisc22@users.noreply.github.com>
2023-03-21 14:40:43 +0200

    added register and login controler, also added user service

---
+++ src/main/java/com/codecool/shop/controller/LoginController.java
@@ -0,0 +1,52 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.DatabaseManager;
+import com.codecool.shop.model.User;
+import com.codecool.shop.service.UserService;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(urlPatterns = {"/login"})
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("product/login.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        User user = new User(req.getParameter("login_email"), req.getParameter("login_password"));
+        UserDao userDao = DatabaseManager.getInstance().getUserDao();
+        UserService userService = new UserService(userDao);
+        String email = req.getParameter("login_email");
+
+        try {
+            if (userDao.findByEmail(email) == null){
+                resp.sendRedirect(req.getContextPath()+"/login");
+            }
+            else {
+                HttpSession session = req.getSession();
+                session.setAttribute("user", email);
+                resp.sendRedirect(req.getContextPath()+ "/");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+}
---
+++ src/main/java/com/codecool/shop/service/UserService.java
@@ -0,0 +1,47 @@
+package com.codecool.shop.service;
+
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.database.UserDaoJdbc;
+import com.codecool.shop.model.User;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class UserService {
+    private static UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public boolean registration(User user) throws SQLException {
+//        if (userDao.findByEmail(user.getEmail()) == null){
+        userDao.add(user);
+        return true;
+//        }
+//        else {
+//
+//            return false;
+//        }
+    }
+
+    public User findUserByEmail(String email) throws SQLException {
+        return userDao.findByEmail(email);
+    }
+
+    public HashMap<String, String> convertToHashMap(User user) {
+        HashMap<String, String> customerData = new HashMap<>();
+        customerData.put("name", user.getName());
+        customerData.put("email", user.getEmail());
+        customerData.put("phone", user.getPhone_number());
+        customerData.put("billingAddress", user.getBilling_address());
+        customerData.put("billingCity", user.getBilling_city());
+        customerData.put("billingZipCode", user.getBilling_zipcode());
+        customerData.put("billingCountry", user.getBilling_country());
+        customerData.put("shippingAddress", user.getShipping_address());
+        customerData.put("shippingCity", user.getShipping_city());
+        customerData.put("shippingZipCode", user.getShipping_zipcode());
+        customerData.put("shippingCountry", user.getShipping_country());
+        return customerData;
+    }
+}
